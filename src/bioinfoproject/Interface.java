9cc025b73fc92e33903c2926d8b83eda890266f3
Kevin DeWire <kevind4272@yahoo.com>
2019-11-18 20:13:00 -0500

    Starting the new project. JFrame for interface created.

--- src/bioinfoproject/Interface.java
+++ src/bioinfoproject/Interface.java
@@ -1,14 +1,30 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Kevin DeWire
+ * CS 723 Bioinformatics
+ * Reduced Representation of a Protein Structure
  */
 package bioinfoproject;
 
-/**
- *
- * @author kevin
- */
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class MyCustomFilter extends javax.swing.filechooser.FileFilter {
+    @Override
+    public boolean accept(File file) {
+        // Allow only directories, or files with ".txt" extension
+        return file.isDirectory() || file.getAbsolutePath().endsWith(".txt") || file.getAbsolutePath().endsWith(".pdb") || file.getAbsolutePath().endsWith(".PDB");
+    }
+    @Override
+    public String getDescription() {
+        // This description will be displayed in the dialog,
+        // hard-coded = ugly, should be done via I18N
+        return "Text documents (*.txt) or PDB files (*.pdb)";
+    }
+}
+
 public class Interface extends javax.swing.JFrame {
 
     /**
@@ -27,21 +43,116 @@ public class Interface extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        FileChooser = new javax.swing.JFileChooser();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        TextArea = new javax.swing.JTextArea();
+        SaveButton = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        Open = new javax.swing.JMenuItem();
+        Exit = new javax.swing.JMenuItem();
+
+        FileChooser.setDialogTitle("");
+        FileChooser.setFileFilter(new MyCustomFilter());
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        TextArea.setColumns(20);
+        TextArea.setRows(5);
+        jScrollPane1.setViewportView(TextArea);
+
+        SaveButton.setText("Save Button");
+        SaveButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SaveButtonActionPerformed(evt);
+            }
+        });
+
+        jMenu1.setText("File");
+
+        Open.setText("Open");
+        Open.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                OpenActionPerformed(evt);
+            }
+        });
+        jMenu1.add(Open);
+
+        Exit.setText("Exit");
+        Exit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ExitActionPerformed(evt);
+            }
+        });
+        jMenu1.add(Exit);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 463, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(SaveButton)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(SaveButton)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 440, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void OpenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OpenActionPerformed
+        int returnVal = FileChooser.showOpenDialog(this);
+            if (returnVal == FileChooser.APPROVE_OPTION) {
+                File InputFile = FileChooser.getSelectedFile();
+                try {
+                  // What to do with the file, e.g. display it in a TextArea
+                  TextArea.read( new FileReader( InputFile.getAbsolutePath() ), null );
+                } catch (IOException ex) {
+                  System.
+                          out.println("problem accessing file"+InputFile.getAbsolutePath());
+                }
+            } else {
+                System.out.println("File access cancelled by user.");
+            }
+    }//GEN-LAST:event_OpenActionPerformed
+
+    private void ExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ExitActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_ExitActionPerformed
+
+    private void SaveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SaveButtonActionPerformed
+        BufferedWriter writer;
+        int returnVal = FileChooser.showSaveDialog(this);
+            if (returnVal == FileChooser.APPROVE_OPTION) {
+                File OutputFile = FileChooser.getSelectedFile();
+                try {
+                  writer = new BufferedWriter( new FileWriter( OutputFile.getAbsolutePath(), false ));
+                  TextArea.write( writer );
+                  writer.close();
+                } catch (IOException ex) {
+                  System.
+                          out.println("problem accessing file"+OutputFile.getAbsolutePath());
+                }
+            } else {
+                System.out.println("File access cancelled by user.");
+            }
+    }//GEN-LAST:event_SaveButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +190,17 @@ public class Interface extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem Exit;
+    private javax.swing.JFileChooser FileChooser;
+    private javax.swing.JMenuItem Open;
+    private javax.swing.JButton SaveButton;
+    private javax.swing.JTextArea TextArea;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
+
+    // Global Variables
+    public String Input;
+    public String Output;
 }
